2706de2bebee3c2b7e09d83144a2158c9908947c
khoa123654 <dangkhoa1574@gmail.com>
2024-06-24 12:17:18 -0700

    Data Clean

---
+++ Dog.java
@@ -0,0 +1,19 @@
+public class Dog {
+    private int dog;
+    private boolean yes;
+
+
+    public Dog(int d, boolean y)
+    {
+        dog = d;
+        yes = y;
+    }
+
+
+    public static void meow()
+    {
+        System.out.println("Alex sucks");
+    }
+
+
+}
